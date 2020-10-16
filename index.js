67d65a0e701d1906fa57ed310e733f3a68379b46
alexrjj <alexsoares2517@gmail.com>
2020-10-16 15:34:51 -0300

    Fix Post relationship

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import slugify from 'slugify';
 import handlebars from 'express-handlebars';
 import connection from './database/connection.js';
 import Author from './authors/Author.js';
+import Post from './posts/Post.js';
 const app = express();
 
 // Database
@@ -44,7 +45,11 @@ app.get('/newAuthor', (req, res) => {
 
 // New Post
 app.get('/newPost', (req, res) => {
-  res.render('newPost');
+  Author.findAll().then(authors => {
+    res.render('newPost', {
+      authors: authors.map(author => author.toJSON())
+    })
+  })  
 });
 
 // ---- POST Routes ----
@@ -59,9 +64,24 @@ app.post('/author/save', (req, res) => {
     email: email
   }).then(() => {
     res.redirect('/newAuthor');
-  });
+  })
 });
 
+// New Post
+app.post('/post/save', (req, res) => {
+  let title = req.body.title;
+  let text = req.body.text;
+  let author = req.body.author;
+
+  Post.create({
+    title: title,
+    text: text,
+    authorId: author,
+    slug: slugify(title, { lower: true })
+  }).then(() => {
+    res.redirect('/');
+  })
+});
 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
