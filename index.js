46e5db8227a9393a644630bd694f03282fe2a447
alexrjj <alexsoares2517@gmail.com>
2020-10-16 10:04:31 -0300

    Config BodyParser, ViewEngine, Static Files

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import express from 'express';
-const app = express();
 import bodyParser from 'body-parser';
 import slutify from 'slugify';
 import handlebars from 'express-handlebars';
+const app = express();
 
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// View Engine
+app.engine('hbs', handlebars( {
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+
+// Static Files
+app.use(express.static('public'));
+
+// ---- Routes ----
+// Home
 app.get('/', (req, res) => {
   res.render('index');
 });
